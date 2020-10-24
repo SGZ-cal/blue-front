8bbf74cdc72ea1c2f8f3456eaae9cb56b611f937
sgz-cal <ls16012j@gmail.com>
2020-10-24 22:11:07 +0900

    show_staffs_index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,52 @@
 <template>
   <div>
-    <button
-      type="button"
-      name="button"
-      @click="getMsg"
-    >
-      RailsからAPIを取得する
-    </button>
-    <div
-      v-for="(msg, i) in msgs"
-      :key="i"
-    >
-      {{ msg }}
+    <h2>
+      Staffsテーブルの取得
+    </h2>
+    <table v-if="staffs.length">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>created_at</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(staff, i) in staffs"
+          :key="`user-${i}`"
+        >
+          <td>{{ staff.id }}</td>
+          <td>{{ staff.name }}</td>
+          <td>{{ staff.email }}</td>
+          <td>{{ dateFormat(staff.created_at) }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div v-else>
+      ユーザーが取得できませんでした
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      msgs: []
-    }
+  async asyncData ({ $axios }) {
+    let staffs = []
+    await $axios.$get('/api/v1/staffs')
+      .then(res => (staffs = res))
+    return { staffs }
   },
-  methods: {
-    getMsg () {
-      this.$axios.$get('/api/v1/hello')
-        .then(res => this.msgs.push(res))
+  computed: {
+    dateFormat () {
+      return (date) => {
+        const dateTimeFormat = new Intl.DateTimeFormat(
+          'ja', { dateStyle: 'medium', timeStyle: 'short' }
+        )
+        return dateTimeFormat.format(new Date(date))
+      }
     }
   }
 }
