a4c2aa370159e4831b5bce92843d5a84757432ac
sgz-cal <ls16012j@gmail.com>
2020-10-28 17:05:03 +0900

    create_signup_layout_files

---
+++ components/beforeLoginHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header-container">
+    <ul class="header-list">
+      <li class="header-item">
+        <a href="/signup">新規登録</a>
+      </li>
+      <li class="header-item">
+        <a href="/login">ログイン</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header-list {
+  width: 100%;
+  height: 80px;
+  line-height: 80px;
+  border-bottom: 0.5px solid gray;
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
---
+++ layouts/beforeLogin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <BeforeLoginHeader />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import BeforeLoginHeader from '@/components/beforeLoginHeader.vue'
+export default {
+  component: {
+    BeforeLoginHeader,
+  }
+}
+</script>
